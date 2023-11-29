5641cbbdcfdc457099af20f89be966f69adf2402
Maurirodiez <c.maurodriguez.a@gmail.com>
2023-11-29 09:03:06 -0300

    Cambios

--- ToDoApp/scripts/signup.js
+++ ToDoApp/scripts/signup.js
@@ -11,10 +11,29 @@ window.addEventListener('load', function () {
     /*            FUNCIÓN 1: Escuchamos el submit y preparamos el envío           */
     /* -------------------------------------------------------------------------- */
     form.addEventListener('submit', function (event) {
-      
+        //Previene que se muestren datos en la url
+        event.preventDefault()
+        // Para armar el objeto de la request que necesita la API
+        const payload = {
+            firstName: nombre.value,
+            lastName: apellido.value,
+            email: email.value,
+            password: password.value
+            }
+        //Configurar la request del FETCH
+        const settings = {
+                method: "POST",
+             //Hay que parsearlo para pasarle los datos al servidor
+                body: JSON.stringify(payload),
+                headers: {
+                    'Content-Type': 'application/json'
+             }
+        }
+        // Esto es para pasarle el settings a la funcion realizarRegister - Lanzar la consulta a la API
+        realizarRegister(settings);
 
-
-
+        //Limpiar los campos del form
+        form.reset()
 
     });
 
@@ -22,8 +41,12 @@ window.addEventListener('load', function () {
     /*                    FUNCIÓN 2: Realizar el signup [POST]                    */
     /* -------------------------------------------------------------------------- */
     function realizarRegister(settings) {
-        
 
+        fetch(`${url}/users`, settings)
+        .then( response => {
+            console.log(response);
+            return response.json()
+        })
 
 
 
